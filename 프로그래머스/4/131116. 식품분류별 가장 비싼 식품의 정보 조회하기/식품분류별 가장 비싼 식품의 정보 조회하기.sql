1d4d38debac2a7f5d3103583fb423d797ad55fef
kdbean <kdbean.dev@gmail.com>
2025-07-09 22:30:19 +0900

    [level 4] Title: 식품분류별 가장 비싼 식품의 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131116. 식품분류별 가장 비싼 식품의 정보 조회하기/식품분류별 가장 비싼 식품의 정보 조회하기.sql
+++ 프로그래머스/4/131116. 식품분류별 가장 비싼 식품의 정보 조회하기/식품분류별 가장 비싼 식품의 정보 조회하기.sql
@@ -1,18 +1,33 @@
-SELECT x1.CATEGORY
-     , x2.MAX_PRICE
-     , x1.PRODUCT_NAME
-  FROM FOOD_PRODUCT x1
-     , (
-         SELECT a.CATEGORY
-         , MAX(a.PRICE) as MAX_PRICE
-           FROM FOOD_PRODUCT a 
-          WHERE 1=1
-            AND REGEXP_LIKE(a.CATEGORY, '과자|국|김치|식용유')
-         GROUP BY a.CATEGORY
-       ) x2 
- WHERE 1=1
-   AND x1.CATEGORY = x2.CATEGORY
-   AND x1.PRICE    = x2.MAX_PRICE
+-- SELECT x1.CATEGORY
+--      , x2.MAX_PRICE
+--      , x1.PRODUCT_NAME
+--   FROM FOOD_PRODUCT x1
+--      , (
+--          SELECT a.CATEGORY
+--          , MAX(a.PRICE) as MAX_PRICE
+--            FROM FOOD_PRODUCT a 
+--           WHERE 1=1
+--             AND REGEXP_LIKE(a.CATEGORY, '과자|국|김치|식용유')
+--          GROUP BY a.CATEGORY
+--        ) x2 
+--  WHERE 1=1
+--    AND x1.CATEGORY = x2.CATEGORY
+--    AND x1.PRICE    = x2.MAX_PRICE
+-- ORDER BY
+--        2 desc
+
+-- RANK 함수 적용
+SELECT a.CATEGORY
+     , a.PRICE as MAX_PRICE
+     , a.PRODUCT_NAME
+  FROM ( select CATEGORY
+              , PRICE
+              , PRODUCT_NAME
+              , rank() over (partition by category order by price desc) as price_rank
+           from food_product
+          where 1=1
+            and regexp_like(CATEGORY, '과자|국|김치|식용유')
+       ) a
+ WHERE a.PRICE_RANK = 1
 ORDER BY
        2 desc
-
