c8033f0f6b2868a60b9f397c186b098414b038ef
kdbean <kdbean.dev@gmail.com>
2025-07-10 23:23:23 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.19 ms, Memory: 31.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Solution {
+    public int solution(int num1, int num2) {
+       
+        double answer = 0;
+
+        if (num1 > 0 && num1 <= 100 && num2 > 0 && num2 <= 100) {            
+            answer = (double) num1 / num2 * 1000;                                
+        }              
+        
+        return (int) answer;      
+    }
+}
