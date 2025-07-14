fcba0fa1388494026557c18d7115d3efac0ddd2c
kdbean <kdbean.dev@gmail.com>
2025-07-14 23:31:56 +0900

    [level 1] Title: Python 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/276013. Python 개발자 찾기/Python 개발자 찾기.sql
+++ 프로그래머스/1/276013. Python 개발자 찾기/Python 개발자 찾기.sql
@@ -4,6 +4,7 @@ select id
      , last_name
   from developer_infos
  where 1=1 
-   and skill_1 = 'Python' or skill_2 = 'Python' or skill_3 = 'Python'
+   /*and skill_1 = 'Python' or skill_2 = 'Python' or skill_3 = 'Python'*/
+   and upper('Python') in (upper(skill_1), upper(skill_2), upper(skill_3))
 order by
        1 asc
